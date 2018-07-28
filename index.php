9bed8bf127e838d9d5ab4fb258104e12442818dc
Pelluro <mipham@student.42.fr>
2018-07-28 12:44:15 +0200

    ajout commande PDO

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-
+$user="pelluro";
+$pass="pelluro";
+try {
+    $dbh = new PDO('mysql:host=spacevanas;port=3307;dbname=pelluro', $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    foreach($dbh->query('SELECT * from toto') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
