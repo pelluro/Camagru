696c80f40d1f7e6b314f2e6bf7b90653aa2f550e
Pelluro <mipham@student.42.fr>
2018-07-31 22:47:45 +0200

    ajout des boutons bootraps,heqder et footer

--- index.php
+++ index.php
@@ -20,10 +20,24 @@ try {
         Toto fait camagru
     </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
-    <body>
+    <br>
 <button class="btn btn-success">Test</button>
-
-ceci est le body
+    <!-- Split button -->
+    <div class="btn-group">
+        <button type="button" class="btn btn-danger">Action</button>
+        <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="caret"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+        </ul>
+    </div>
+</br>ceci est le body</br>
 </body>
 </head>
 
