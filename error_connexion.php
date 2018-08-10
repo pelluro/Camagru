95a6f8ac62e6b33533460ac5266d086fa59a7248
Minh PHAM <mipham@e1r6p6.42.fr>
2018-08-10 10:54:33 +0200

    ok

---
+++ error_connexion.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mipham
+ * Date: 8/10/18
+ * Time: 10:54 AM
+ */
--- login.php
+++ login.php
@@ -12,29 +12,23 @@ else
     $passwd = htmlentities($_POST['passwd'], ENT_QUOTES,"UTF-8");
     $passwd = hash('whirpool', $passwd);
     connect();
-}
-$req = "SELECT email, login, passwd FROM users";
-$query = $bdd->prepare($req);
-$query->execute();
-if ($query->rowCount() > 0)
-{
-    while ($data = $query->fetch())
-    {
-        if ($login === $data['login'] AND $passwd === $data['passwd'])
-        {
-            $_SESSION['login'] = $login;
-            $email = $data['email'];
-            $_SESSION['email'] = $email;
-            header('location: main.php');
-            exit;
+    $req = "SELECT email, login, passwd FROM users";
+    $query = $bdd->prepare($req);
+    $query->execute();
+    if ($query->rowCount() > 0) {
+        while ($data = $query->fetch()) {
+            if ($login === $data['login'] AND $passwd === $data['passwd']) {
+                $_SESSION['login'] = $login;
+                $email = $data['email'];
+                $_SESSION['email'] = $email;
+                header('location: main.php');
+                exit;
+            } else
+                header('location: err_connexion.php');
         }
-        else
-            header('location: err_connexion.php');
     }
+    else
+        header('location: err_connexion.php');
+    $query->closeCursor();
 }
-else
-    header('location: err_connexion.php');
-$query->closeCursor();
-}
-
 ?>
