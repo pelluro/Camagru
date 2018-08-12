e367139c4a2aeca1238b1e775174510ba356afab
Pelluro <mipham@student.42.fr>
2018-08-13 00:25:21 +0200

    ajout approuve

---
+++ approuve.php
@@ -0,0 +1,33 @@
+<?php
+$titlePage = "approuve";
+include('./header.php');
+
+if (!$_GET['login'] || !$_GET['confirmation_code'] || !$_GET['email'])
+    header('location: index.php');
+else
+{
+    $code=$_GET['confirmation_code'];
+    connect();
+    $req = "SELECT * FROM comments WHERE com_confirmation_code='?' ";
+    $query = $bdd->prepare($req);
+    $query->execute( array($code));
+    if ($query->rowCount() == 1)
+    {
+        $req = "UPDATE comments SET active='1' WHERE com_confirmation_code='?' ";
+        $query = $bdd->prepare($req);
+        $result = $query->execute( array($code));
+        if ($result)
+        {
+            header('location: photos.php');
+        }
+        else
+            echo "<p>Sorry, there has been a problem approving this comment. Please contact admin.</p>";
+    }
+    else
+        header('location: index.php');
+    $query->closeCursor();
+}
+
+
+include('./footer.php');
+?>
--- error_connexion.php
+++ error_connexion.php
@@ -3,7 +3,18 @@
     include('./header.php');
 ?>
 <div class="alert-danger">
-    <h1> Could not Sign In</h1>
+        <h1>Registration failed. Try again!</h1>
+        <p>Please fill in all the fields</p>
+        <div class ="formul">
+            <form id="form" action="register.php" method="POST">
+                <p class="titre"> </p>
+                E-mail : <input type="email" name="email" value=""/>
+                <br />
+                Login : <input type="text" name="login" size="30" /><br />
+                Password (min 8 characters) : <input type="password" name="passwd" size="30" /><br />
+                <input id="reset" type="submit" name="submit" value="Register" style="width: 223px;">
+            </form>
+        </div>
 </div>
 <?php
 include('./footer.php');
