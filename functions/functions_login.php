9f39ac57dcd721f585a60c84fff9a35d9fc14b10
Minh PHAM <mipham@e2r5p4.42.fr>
2018-08-20 18:41:38 +0200

    envoie mail semi reussi

--- functions/functions_login.php
+++ functions/functions_login.php
@@ -2,34 +2,25 @@
 function mail_confirmation($email, $login, $guid)
 {
     $subject = "Email confirmation";
-    $headers = 'Content-Type: text/html; charset=ISO-8859-1' . "\r\n" . 'From: noreply@camagru.com' . "\r\n" . 'X-Mailer: PHP/' .phpversion();
+    $headers = 'From: Minh PHAM <no-reply@camagru.com>'."\r\n";
+    $headers .= 'X-Mailer: PHP/' . phpversion() . "\r\n";
+    $headers .= 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $message ="Bienvenue sur Camagru!";
     ?>
-    <table class="w580" width="580" cellpadding="0" cellspacing="0" border="0">
-        <tr>
-            <td class="w580" width="580">
-                <h2 style="color:#0E7693; font-size:22px; padding-top:12px;"> Account verification  </h2>
-                <div align="left" class="article-content">
-                    <p>Hi '.$login.'</p>
-                    <p>Please click on the below link to verify your account</p>
-                    <a href="<?=str_replace("register","confirmation",$_SERVER["HTTP_REFERER"])?>?login='<?=$login?>'&email='<?=$email?>'&confirmation_code='{<?=$guid?>}'">Confirm my account</a>
-                    <p>You will not have access to camagru until you click on the above link</p>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="w580" width="580" height="1" bgcolor="#c7c5c5"></td>
-        </tr>
-    </table>
+     <h2> Account verification</h2>
+        <p>Hi <?=$login?></p>
+        <p>Please click on the below link to verify your account</p>
+    <a href="http://127.0.0.1:8100/Camagru/confirmation.php?login='<?=$login?>'&email='<?=$email?>'&confirmation_code='{<?=$guid?>}'">Confirm my account</a>
+        <p>You will not have access to camagru until you click on the above link</p>
     <?php
-    mail($email, $subject, $message, $headers);
+    var_dump(mail($email, $subject, $message, $headers));
 }
-
 
 function mail_password($email, $login, $rand)
 {
 $subject = "Reset Your Password";
-$headers = 'Content-Type: text/html; charset=ISO-8859-1' . "\r\n" . 'From: noreply@camagru.com' . "\r\n" . 'X-Mailer: PHP/' .phpversion();
+$headers = 'From: Minh PHAM <no-reply@camagru.com>'."\r\n";
 $message="";
 ?>
 <html>
