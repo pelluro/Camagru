531389e13a81dcfce1a84d47afe816b71ff73ec8
pelluro <mipham@student.42.fr>
2018-09-12 15:13:00 +0200

    ajout setup docker dans login ecole

--- config/database.php
+++ config/database.php
@@ -1,8 +1,7 @@
 <?php
-//require_once "login_ecole.php";
+require_once "login_ecole.php";
 //require_once "login_nas.php";
 //require_once "login_surface.php";
-require_once "login_docker.php";
 try {
     $dbConnection = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- config/database_setup.php
+++ config/database_setup.php
@@ -1,8 +1,7 @@
 <?php
-//require_once "login_ecole_setup.php";
+require_once "login_ecole_setup.php";
 //require_once "login_nas_setup.php";
 //require_once "login_surface_setup.php";
-require_once "login_docker.php";
 try {
     $dbConnection = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
